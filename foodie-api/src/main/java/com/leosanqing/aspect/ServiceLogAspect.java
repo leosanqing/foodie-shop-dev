ebdc22459974667667490250ba2154b4fc5e1c4f
leosanqing <stormleo@qq.com>
2020-11-28 15:07:11 +0800

    1. update 格式化代码

--- foodie-api/src/main/java/com/leosanqing/aspect/ServiceLogAspect.java
+++ foodie-api/src/main/java/com/leosanqing/aspect/ServiceLogAspect.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -29,13 +27,14 @@ public class ServiceLogAspect {
 
     /**
      * 切面表达式
-     *
+     * <p>
      * 第一处 * 代表方法返回类型 * 代表所有类型
      * 第二次 包名  代表aop监控的包
      * 第三处 .. 代表该包及其子包下的所有类方法
      * 第四处 * 代表类名 *代表所有类
      * 第五处 . 表示引用的点
      * 第六处 *(..) * 代表类名，(..)表示方法中的任何参数
+     *
      * @param joinPoint
      * @return
      * @throws Throwable
@@ -58,12 +57,12 @@ public class ServiceLogAspect {
         //结束时间
         long end = System.currentTimeMillis();
         long takeTime = end - begin;
-        if(takeTime > 3000){
-            log.error("===== 执行结束 耗时: {} 毫秒=====",takeTime);
-        }else if(takeTime > 2000){
-            log.warn("===== 执行结束 耗时: {} 毫秒=====",takeTime);
-        }else{
-            log.info("===== 执行结束 耗时: {} 毫秒=====",takeTime);
+        if (takeTime > 3000) {
+            log.error("===== 执行结束 耗时: {} 毫秒=====", takeTime);
+        } else if (takeTime > 2000) {
+            log.warn("===== 执行结束 耗时: {} 毫秒=====", takeTime);
+        } else {
+            log.info("===== 执行结束 耗时: {} 毫秒=====", takeTime);
         }
 
         return result;
